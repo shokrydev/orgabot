6793117085d3648be82633060bc1d8a529e5e84d
shokrydev <m@shokry.dev>
2025-11-07 23:46:20 +0100

    Modularize vlm_prompter.py and replace DocumentModel with categories list argument

--- vlm_prompter.py
+++ vlm_prompter.py
@@ -1,38 +1,46 @@
 from ollama import generate
-from pydantic import BaseModel
-        
-class DocumentModel(BaseModel):
-    language: str
-    genre: str
-    subgenre: str
-    subject: str
+from typing import List
 
-def get_category(file_name: str, thumbnail_path: str, model: str = 'qwen2.5vl:7b') -> str:  
-    """Prompts VLM with filename and thumbnail to generate file category.
+class Prompter:
+    def __init__(self, categories: List[str], model: str = 'qwen2.5vl:7b'):
+        self.categories = categories
+        self.system_prompt = f'You are an expert file categorizer. Given a filename and thumbnail, respond only to which one of the following categories the file belongs: {", ".join(self.categories)}'
+        self.model = model
 
-    Args:
-        file_name (str): Name of file to catogerize.
-        thumbnail_path (str): Path to file's thumbnail.
-        model (str): Name of VLM in Ollama (default: 'qwen2.5vl:7b')
+    def get_category(self, file_name: str, thumbnail_path: str) -> str:  
+        """Prompts VLM with filename and thumbnail to generate file category.
 
-    Returns:
-        predicted_category (str): File category predicted by VLM based on filename and thumbnail.
-    """
+        Args:
+            file_name (str): Name of file to catogerize.
+            thumbnail_path (str): Path to file's thumbnail.
 
-    predicted_category = generate(
-        model=model,
-        system='You are an expert file categorization assistant. Given a filename and thumbnail, categorize the file into a single-word book genre, subgenre and language.',
-        prompt=file_name,
-        images=[thumbnail_path],
-        stream=False,
-        format=DocumentModel.model_json_schema(),
-    )['response']
+        Returns:
+            predicted_category (str): File category from self.categories predicted by VLM based on filename and thumbnail.
+        """
 
-    return predicted_category
+        predicted_category = generate(
+            model=self.model,
+            system=self.system_prompt,
+            prompt=file_name,
+            images=[thumbnail_path],
+            stream=False,
+        )['response']
+
+        assert predicted_category in self.categories, f"Predicted category '{predicted_category}' not in predefined categories."
+
+        return predicted_category
 
 
 if __name__ == "__main__":
     import thumbnail_curation
     dic = thumbnail_curation.get_file_thumbnail_dict(files_dir_path = '~/Documents/papers')
+
+    categories  = [ '3D Vision', 'Computer Vision', 'Natural Language Processing', 'Self-Supervised Learning', 'Generative AI', 'Remote Sensing', 'Reinforcement Learning']
+
+    prompter = Prompter(categories=categories)
+
     for fname, thumb_path in dic.items():
-       print(fname, get_category(fname,thumb_path))
+        if thumb_path is not None:
+            print(fname, '---', prompter.get_category(fname,thumb_path))
+        else:
+            print(fname, '---' , 'No thumbnail provided.')
